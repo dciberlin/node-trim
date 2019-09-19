a8d651665b0d18af9b1fe9dd2f7fad7aae3fe8a4
moequan <maurice.s93@web.de>
2019-09-19 10:07:02 +0200

    Fixing issues.(sanitize

--- formatting.js
+++ formatting.js
@@ -1,12 +1,10 @@
 module.exports.capitalizeInitial = (str) => {
 
-   
+
     str = str.split(` `)
-  let format =  str.map(x=> x.charAt(0).toUpperCase()+x.substring(1).toLowerCase())
-  format = format.join(` `)
-  let formAndTrim = format.replace(/\s+/g, "");
-  formAndTrim = formAndTrim.replace(/[^a-zA-Z0-9]/g, ' ') 
-  console.log(formAndTrim)
+    let format = str.map(x => x.charAt(0).toUpperCase() + x.substring(1).toLowerCase())
+    format = format.join(` `)
+    let formAndTrim = format.replace(/\s+/g, " ");
+    formAndTrim = formAndTrim.replace(/[^a-zA-Z0-9]/g, ' ')
+    console.log(formAndTrim)
 }
-
-
--- messaging.js
+++ messaging.js
@@ -1,5 +1,9 @@
-module.exports.showHelp = (help) =>{
-    if(help === `--help`){
-        return console.log(`Do you need help?!`)
+module.exports.showHelp = (str,help) => {
+    if (help === `--help`) {
+
+        str = str.split(` `)
+    let format = str.map(x => x.charAt(0).toUpperCase() + x.substring(1).toLowerCase()).join(` `).replace(/\s+/g, " ")
+
+    
     }
 }
